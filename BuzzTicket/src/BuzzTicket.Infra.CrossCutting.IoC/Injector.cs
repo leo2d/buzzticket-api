f91a1e083c0564d041b59bab54535ac3c042bdf2
Leonardo Dias <lcdias1@latam.stefanini.com>
2019-10-28 13:25:17 -0300

    configuracao IoC e Di

---
+++ BuzzTicket/src/BuzzTicket.Infra.CrossCutting.IoC/Injector.cs
@@ -0,0 +1,25 @@
+﻿using BuzzTicket.Domain.SharedKernel.Interfaces;
+using BuzzTicket.Domain.TicketAgg.Contracts;
+using BuzzTicket.Domain.TicketAgg.Services;
+using BuzzTicket.Infra.Data.Config;
+using BuzzTicket.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuzzTicket.Infra.CrossCutting.IoC
+{
+    public class Injector
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+                    options.UseMySQL(configuration.GetConnectionString("ConnectionStrings:MySqlConnectionString")));
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            
+            services.AddTransient<ITicketService, TicketService>();
+            services.AddTransient<ITicketRepository, TicketRepository>();
+        }
+    }
+}
