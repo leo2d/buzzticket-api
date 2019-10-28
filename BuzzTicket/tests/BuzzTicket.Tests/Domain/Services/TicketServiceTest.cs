302df46c9ef277d5f13283bbd519953284e35f25
Leonardo Dias <lcdias1@latam.stefanini.com>
2019-10-28 16:12:09 -0300

    Criacao estrutura de testes Unitarios

---
+++ BuzzTicket/tests/BuzzTicket.Tests/Domain/Services/TicketServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using BuzzTicket.Domain.TicketAgg;
+using BuzzTicket.Tests.TestConfiguration.Builders;
+using BuzzTicket.Tests.TestConfiguration.Mocks;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BuzzTicket.Tests.Domain.Services
+{
+    public class TicketServiceTest
+    {
+
+        private TicketServiceMock _serviceMock;
+
+        public TicketServiceTest()
+        {
+            _serviceMock = new TicketServiceMock();
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task BuscarBuscarTicketsOrdenadosPorMaiorDataSuscessoTests(IEnumerable<Ticket> tickets, int quantidade)
+        {
+            #region Given
+
+            var service = _serviceMock
+                .MockBuscarTicketsOrdenadosPorMaiorData(tickets: tickets, quantidade: quantidade)
+                .Resolve();
+
+            #endregion
+
+            #region When
+
+            var result = await service.BuscarTicketsOrdenadosPorMaiorData(quantidade);
+
+            #endregion
+
+            #region Then
+
+            result.Should().NotBeEmpty();
+            result.Should().HaveCount(quantidade);
+            result.Should().NotBeNull();
+
+            #endregion
+
+        }
+
+        public static IEnumerable<object[]> TestData => new List<object[]>()
+        {
+            new object[]
+            {
+               TicketBuilder.Build(1) , 1
+            },
+            new object[]
+            {
+                TicketBuilder.Build(2), 2
+            },
+            new object[]
+            {
+                TicketBuilder.Build(20), 20
+            },
+        };
+    }
+}
---
+++ BuzzTicket/tests/BuzzTicket.Tests/TestConfiguration/Mocks/CustomMock.cs
@@ -0,0 +1,35 @@
+﻿using AutoMoqCore;
+
+namespace BuzzTicket.Tests.TestConfiguration.Mocks
+{
+
+    /// <summary>
+    ///     Classe base para extensão de um serviço à ser mockado
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class CustomMock<T> where T : class
+    {
+        public AutoMoqer _mocker;
+
+        protected CustomMock()
+        {
+            _mocker = new AutoMoqer();
+        }
+
+        /// <summary>
+        ///     Obriga o usuário a realizar a criação dos mocks à serem utilizados em cada serviço testado com retorno padrão
+        /// </summary>
+        /// <returns></returns>
+        public abstract CustomMock<T> UseDefaultMock();
+
+        /// <summary>
+        ///     Concretiza a instância do serviço à ser testado
+        /// </summary>
+        /// <returns></returns>
+        public T Resolve()
+        {
+            return _mocker.Resolve<T>();
+        }
+    }
+
+}
