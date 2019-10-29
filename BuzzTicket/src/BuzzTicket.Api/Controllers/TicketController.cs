845f965f55481b73a3066b480d3e35f317878852
Leonardo Dias <contato.leonardod@yahoo.com>
2019-10-28 23:30:08 -0300

    criacao viewmodel e controller

--- BuzzTicket/src/BuzzTicket.Api/Controllers/TicketController.cs
+++ BuzzTicket/src/BuzzTicket.Api/Controllers/TicketController.cs
@@ -1,8 +1,8 @@
-﻿using BuzzTicket.Domain.TicketAgg;
+﻿using BuzzTicket.Api.ViewModels;
+using BuzzTicket.Domain.TicketAgg;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BuzzTicket.Api.Controllers
@@ -13,9 +13,34 @@ namespace BuzzTicket.Api.Controllers
     {
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> BuscarTodos()
         {
             return Ok(new List<Ticket>() { new Ticket() { Aberto = true, Solicitacao = "sdad", Solicitante = "qwdqwdqwdqdw" } });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar([FromBody] TicketViewModel ticketViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest("Dados informados são inválidos");
+
+            return Created("", "");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Atualizar([FromBody] TicketViewModel ticketViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest("Dados informados são inválidos");
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Excluir([FromQuery] Guid id)
+        {
+            if (id == null || id == Guid.Empty) return BadRequest("Dados informados são inválidos");
+
+            return NoContent();
+        }
+
     }
 }
---
+++ BuzzTicket/src/BuzzTicket.Api/ViewModels/TicketViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuzzTicket.Api.ViewModels
+{
+    public class TicketViewModel
+    {
+        public Guid Id { get; set; }
+        public bool Aberto { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Solicitante { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [MaxLength(1000)]
+        public string Solicitacao { get; set; }
+    }
+}
